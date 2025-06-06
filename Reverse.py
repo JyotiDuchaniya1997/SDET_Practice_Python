2882f9321ad61f1137d713e30c6507df186f8c2c
JyotiDuchaniya1997 <jyoti09061998@gmail.com>
2025-06-07 01:16:58 +0530

    added reverse.py

--- Reverse.py
+++ Reverse.py
@@ -1,4 +1,11 @@
 s='hello lets do it'
-# reverse each character
-for x in range(length(s)-1,0):
+# reverse each character without changing index
+print(s[::-1])
+#reverse based on space
+words= s.split(" ")
+print(words)
+print(' '.join(words[::-1]))
+# reverse each chacter by chaging index
+s1=s[::-1]
+print(s,"\n",s1)
     
